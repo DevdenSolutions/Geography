90df914e83290ceab33b27bb61968ab92f3d53b6
RamKumarS07 <68840254+DevdenSolutions@users.noreply.github.com>
2022-01-13 20:45:19 +0530

    UI functionalities
    Done UI
    Left Resetting

--- Assets/Scripts/Functions/CycleThroughLean.cs
+++ Assets/Scripts/Functions/CycleThroughLean.cs
@@ -1,5 +1,9 @@
 ﻿using Lean.Gui;
+using Lean.Transition.Method;
+using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CycleThroughLean : MonoBehaviour
 {
@@ -7,11 +11,22 @@ public class CycleThroughLean : MonoBehaviour
     LeanToggle[] Slides;
     int SlideCount;
     int currentSlide = 0;
-
+    public Transform listParent;
+    public LeanGameObjectSetActive leanGameObjectSetActiveTransition;
+    public List<string> title;
     private void Start()
     {
         SlideCount = Slides.Length;
         Slides[0].TurnOn();
+
+        for (int i = 0; i < Slides.Length; i++)
+        {
+            int k = i;
+            listParent.GetChild(k).GetComponent<Button>().onClick.AddListener(() => { TurnSlideOn(k); print(123); });
+            listParent.GetChild(k).GetChild(0).GetComponent<TextMeshProUGUI>().text = (k + 1) + ". " + title[k];
+        }
+        ChangeListBgColor();
+
     }
 
     public void GoForward()
@@ -19,8 +34,10 @@ public class CycleThroughLean : MonoBehaviour
         if (currentSlide < SlideCount-1)
         {
             currentSlide++;
+
             TurnSlideOn(currentSlide);
         }
+        ChangeListBgColor();
     }
 
     public void GoBackward()
@@ -30,10 +47,33 @@ public class CycleThroughLean : MonoBehaviour
             currentSlide--;
             TurnSlideOn(currentSlide);
         }
+        ChangeListBgColor();
     }
 
     void TurnSlideOn(int index)
     {
+        currentSlide = index;
         Slides[index].TurnOn();
+        ChangeListBgColor();
+        leanGameObjectSetActiveTransition.BeginAllTransitions();
     }
+
+
+    public void ChangeListBgColor()
+    {
+        for (int i = 0; i < Slides.Length; i++)
+        {
+            listParent.GetChild(i).GetChild(0).GetComponent<LeanGraphicColor>().BeginThisTransition();
+        }
+        listParent.GetChild(currentSlide).GetComponent<LeanGraphicColor>().BeginThisTransition();
+        print(000);
+    }
+
+    public void ChangeCurrentBgColor()
+    {
+        listParent.GetChild(currentSlide).GetComponent<LeanGraphicColor>().BeginThisTransition();
+        print(000);
+    }
+
+
 }
--- Assets/Scripts/Slides/Mountains.cs
+++ Assets/Scripts/Slides/Mountains.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using WPM;
@@ -6,6 +7,8 @@ using WPM;
 public class Mountains : MonoBehaviour
 {
     WorldMapGlobe map;
+   
+
     private void Awake()
     {
         map = WorldMapGlobe.instance;
@@ -13,8 +16,17 @@ public class Mountains : MonoBehaviour
     private void OnEnable()
     {
         map.FlyToCountry(map.GetCountryIndex("India"));
-        map.SetZoomLevel(.5f);
         map.earthStyle = EARTH_STYLE.NaturalHighRes16K;
         
+    }
+
+    public void ZoomIN()
+    { 
+        map.SetZoomLevel(.5f);
+    }
+    public void ZoomOut()
+    { 
+        map.SetZoomLevel(1.4f);
+
     }
 }
